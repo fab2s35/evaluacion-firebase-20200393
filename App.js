2a7bc1c250c2239b9728fd3d9055c5c1fdd028b7
fab2s35 <sosorto72@gmail.com>
2025-09-03 10:00:52 -0600

    avances en la conexion con firebase

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 import Navigation from './src/navigation/Navigation';
+import { API_KEY } from '@env';
+
 export default function App() {
+  useEffect(() => {
+    console.log("API_KEY desde @env:", API_KEY);
+  }, []);
+
   return (
     <Navigation />
   );
